44cb608229fa167c3d40a62d93197af49ae72d0c
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-06-21 17:30:59 -0300

    56. Correção no Componente MineField

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
     minesAmount = () => {
         const cols = params.getColumnsAmount();
         const rows = params.getRowsAmount();
-        return Math.ceil(cols * row * params.difficultLevel);
+        return Math.ceil(cols * rows * params.difficultLevel);
     };
 
     createState = () => {
@@ -31,6 +31,9 @@ export default class App extends Component {
                     Tamanho da grade: {params.getRowsAmount()}x
                     {params.getColumnsAmount()}
                 </Text>
+                <View style={styles.board}>
+                    <MineField board={this.state.board} />
+                </View>
             </View>
         );
     }
@@ -39,18 +42,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#F5FCFF"
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: "center",
-        margin: 10
-    },
-    instructions: {
-        textAlign: "center",
-        color: "#333333",
-        marginBottom: 5
-    }
+        justifyContent: "flex-end",
+	},
+	board: {
+		alignItems: 'center',
+		backgroundColor: '#aaa'
+	}
 });
--- src/components/MineField.js
+++ src/components/MineField.js
@@ -3,19 +3,24 @@ import { View, StyleSheet } from "react-native";
 import Field from "./Field";
 
 export default props => {
-    const rows = props.board.map((row, r) => { // retorna as linhas com as colunas dentro
-        const columns = row.map((field, c) => { // nas colunas tem a unidade com os atributos de cada campo
+    const rows = props.board.map((row, r) => {
+        // retorna as linhas com as colunas dentro
+        const columns = row.map((field, c) => {
+            // nas colunas tem a unidade com os atributos de cada campo
             return <Field {...field} key={c} />;
         });
-        return <View key={r}>{columns}</View>;
+        return (
+            <View key={r} style={{ flexDirection: "row" }}>
+                {columns}
+            </View>
+        );
     });
     return <View style={styles.container}>{rows}</View>;
 };
 
-
 const styles = StyleSheet.create({
-	container: {
-		flexDirection: 'row',
-		backgroundColor: '#eee',
-	}
-})
+    container: {
+        // flexDirection: 'row',
+        backgroundColor: "#eee"
+    }
+});
