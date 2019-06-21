2df7303b551744be31a2ab9a26ce61b626b7a23f
Matheus de Souza <matheus.zhsouza@gmail.com>
2019-06-21 17:30:38 -0300

    57. Explicando a Lógica do Jogo

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import params from "./src/params";
+import MineField from "./src/components/MineField";
+import { createMinedBoard } from "./src/functions";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = this.createState();
+    }
+
+    minesAmount = () => {
+        const cols = params.getColumnsAmount();
+        const rows = params.getRowsAmount();
+        return Math.ceil(cols * row * params.difficultLevel);
+    };
+
+    createState = () => {
+        const cols = params.getColumnsAmount();
+        const rows = params.getRowsAmount();
+        return {
+            board: createMinedBoard(rows, cols, this.minesAmount())
+        };
+    };
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Iniciando o Mines!</Text>
+                <Text style={styles.instructions}>
+                    Tamanho da grade: {params.getRowsAmount()}x
+                    {params.getColumnsAmount()}
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F5FCFF"
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: "center",
+        margin: 10
+    },
+    instructions: {
+        textAlign: "center",
+        color: "#333333",
+        marginBottom: 5
+    }
+});
---
+++ src/components/MineField.js
@@ -0,0 +1,21 @@
+import React from "React";
+import { View, StyleSheet } from "react-native";
+import Field from "./Field";
+
+export default props => {
+    const rows = props.board.map((row, r) => { // retorna as linhas com as colunas dentro
+        const columns = row.map((field, c) => { // nas colunas tem a unidade com os atributos de cada campo
+            return <Field {...field} key={c} />;
+        });
+        return <View key={r}>{columns}</View>;
+    });
+    return <View style={styles.container}>{rows}</View>;
+};
+
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		backgroundColor: '#eee',
+	}
+})
